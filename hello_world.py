614969135d2ca39439c54828b883eb60369d7089
Latemaster <100564327+Latemaster@users.noreply.github.com>
2026-02-20 15:10:24 +0200

    Fix merge conflict in hello_world.py
    Resolved merge conflict and updated print statement.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 print ("Hello world!")
-=======
-print("Hello world!")
-<<<<<<< HEAD
->>>>>>> int-normal-merge/master
-=======
 print("Hello once again")
->>>>>>> int-normal-merge/feature/add-logging
+
